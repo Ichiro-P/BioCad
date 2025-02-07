fa86934e8434bf191e14241837f3b23e3ae3d2fb
Pedro N <96268046+Ichiro-P@users.noreply.github.com>
2025-02-07 13:02:33 -0300

    Add files via upload

--- build/CMakeFiles/BioCad.dir/DependInfo.cmake
+++ build/CMakeFiles/BioCad.dir/DependInfo.cmake
@@ -8,12 +8,12 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/itiro/Documentos/BioCad/src/clienteDAO.cpp" "CMakeFiles/BioCad.dir/src/clienteDAO.cpp.obj" "gcc" "CMakeFiles/BioCad.dir/src/clienteDAO.cpp.obj.d"
-  "C:/Users/itiro/Documentos/BioCad/src/clienteDVO.cpp" "CMakeFiles/BioCad.dir/src/clienteDVO.cpp.obj" "gcc" "CMakeFiles/BioCad.dir/src/clienteDVO.cpp.obj.d"
-  "C:/Users/itiro/Documentos/BioCad/src/loginManager.cpp" "CMakeFiles/BioCad.dir/src/loginManager.cpp.obj" "gcc" "CMakeFiles/BioCad.dir/src/loginManager.cpp.obj.d"
-  "C:/Users/itiro/Documentos/BioCad/src/main.cpp" "CMakeFiles/BioCad.dir/src/main.cpp.obj" "gcc" "CMakeFiles/BioCad.dir/src/main.cpp.obj.d"
-  "C:/Users/itiro/Documentos/BioCad/src/telaManager.cpp" "CMakeFiles/BioCad.dir/src/telaManager.cpp.obj" "gcc" "CMakeFiles/BioCad.dir/src/telaManager.cpp.obj.d"
-  "C:/Users/itiro/Documentos/BioCad/src/usuarioDAO.cpp" "CMakeFiles/BioCad.dir/src/usuarioDAO.cpp.obj" "gcc" "CMakeFiles/BioCad.dir/src/usuarioDAO.cpp.obj.d"
+  "/home/alunos/a2650517/Área de trabalho/BioCad-main/src/clienteDAO.cpp" "CMakeFiles/BioCad.dir/src/clienteDAO.cpp.o" "gcc" "CMakeFiles/BioCad.dir/src/clienteDAO.cpp.o.d"
+  "/home/alunos/a2650517/Área de trabalho/BioCad-main/src/clienteDVO.cpp" "CMakeFiles/BioCad.dir/src/clienteDVO.cpp.o" "gcc" "CMakeFiles/BioCad.dir/src/clienteDVO.cpp.o.d"
+  "/home/alunos/a2650517/Área de trabalho/BioCad-main/src/loginManager.cpp" "CMakeFiles/BioCad.dir/src/loginManager.cpp.o" "gcc" "CMakeFiles/BioCad.dir/src/loginManager.cpp.o.d"
+  "/home/alunos/a2650517/Área de trabalho/BioCad-main/src/main.cpp" "CMakeFiles/BioCad.dir/src/main.cpp.o" "gcc" "CMakeFiles/BioCad.dir/src/main.cpp.o.d"
+  "/home/alunos/a2650517/Área de trabalho/BioCad-main/src/sistemaManager.cpp" "CMakeFiles/BioCad.dir/src/sistemaManager.cpp.o" "gcc" "CMakeFiles/BioCad.dir/src/sistemaManager.cpp.o.d"
+  "/home/alunos/a2650517/Área de trabalho/BioCad-main/src/usuarioDAO.cpp" "CMakeFiles/BioCad.dir/src/usuarioDAO.cpp.o" "gcc" "CMakeFiles/BioCad.dir/src/usuarioDAO.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
